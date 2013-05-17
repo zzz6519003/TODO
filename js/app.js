302ae30c3ecdd795d576b33c41db676ac1a01645
zzz6519003 <zhengzhongzhao@gmail.com>
2013-05-17 12:23:03 +0800

    wow

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 // js/app.js
 
   var app = app || {};
+
+  app.AppView = Backbone.View.extend({
+  	el: '#todoapp',
+  	statsTemplate: _.template($('#stats-template').html()),
+  	initialize: function() {
+  		this.allCheckbox = this.$('#toggle-all')[0];
+  		this.$input = this.$('#new-todo');
+  		this.$footer = this.$('#footer');
+  		this.$main = this.$('#main');
+
+  		this.listenTo(app.Todos, 'add', this.addOne);
+  		this.listenTo(app.Todos, 'reset', this.addAll);
+  	}，
+
+  	addOne: function(todo) {
+  		var view = new app.TodoView({model: todo});
+  		$('#todo-list').append(view.render().el)
+  	},
+
+  	addAll: function() {
+  		this.$('#todo-list').html('');
+  		app.Todos.each(this.addOne, this);
+  	}
+  });
   var ENTER_KEY = 13;
 
   $(function() {
